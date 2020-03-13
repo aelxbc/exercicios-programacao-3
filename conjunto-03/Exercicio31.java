6db9a3f94aad1f68dd96f1d6007868e6633fe99f
Alex Corrêa <alexxx.nada@gmail.com>
2020-03-13 13:59:43 -0300

    Exercício 31

---
+++ conjunto-03/Exercicio31.java
@@ -0,0 +1,13 @@
+/**
+ * Exiba mil vezes o número 100.
+ */
+
+ public class Exercicio31 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("[" + i + "] =" + " 100");
+        }
+        
+    }
+}
