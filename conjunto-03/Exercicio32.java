8b071741326bcb6567bd74bd3739670867a5e14e
Alex Corrêa <alexxx.nada@gmail.com>
2020-03-13 14:05:02 -0300

    Exercício 32

---
+++ conjunto-03/Exercicio32.java
@@ -0,0 +1,13 @@
+/**
+ * Exiba todos os números de 1 a 500, um ao lado do outro com um espaço em branco de separação 
+ */
+
+ public class Exercicio32 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 500; i++) {
+            System.out.print((i+1) + " ");
+        }
+        
+    }
+}
