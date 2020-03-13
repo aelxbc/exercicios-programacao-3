f40401c9d63a5664324d044e7f35b20d4b9d8213
Alex Corrêa <alexxx.nada@gmail.com>
2020-03-13 14:12:20 -0300

    Exercício 33

---
+++ conjunto-03/Exercicio33.java
@@ -0,0 +1,13 @@
+/**
+ * Exiba todos os números pares de 10 a 200.
+ */
+
+ public class Exercicio33 {
+    public static void main(String[] args) {
+
+        for (int i = 10; i <= 200; i+=2) {
+            System.out.println(i);
+        }
+        
+    }
+}
